797c54c6b3972ed9e57c8a1c406879374e87f1be
Jessie7887 <98558991+Jessie7887@users.noreply.github.com>
2024-06-10 14:03:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,16 +14,15 @@ def order():
     mastermind.game()   # play game
 
 # idea for strikethrough text....
-def strike(text):
-    result = ''
-    for c in text:
-        result = (f'{result}{c}\u0336')
-        return result
+# def strike(text):
+#     result = ''
+#     for c in text:
+#         result = (f'{result}{c}\u0336')
+#         return result
 
 
-# Press the green button in the gutter to run the script.
+# Run the program.
 if __name__ == '__main__':
-    # print(strike("greeeeeen"))
     order()
 
 
